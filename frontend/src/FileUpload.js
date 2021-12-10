4774e2475aaf2486986b6d5d3efd369da4ea46ae
hasanarcas <hasanarcas@gmail.com>
2021-12-10 21:18:59 +0300

    Added frontend

---
+++ frontend/src/FileUpload.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function UploadForm() {
+    return (
+        <div className='UploadForm'>
+            <h1>Form</h1>
+        </div>
+    )
+}
+
+export default UploadForm
+
