6f0f63dc6f22b1cb4bb2eb632f573745543a59cf
hasanarcas <hasanarcas@gmail.com>
2021-12-12 12:09:47 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import UploadForm from './FileUpload';
-import Home from "./Home"
+import Home from "./Home";
+import Navigator from './NavigationBar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World </h1>
-      <UploadForm/>
-      <Home />
+        <Router>
+        <Navigator />
+          <Routes>
+            <Route path="/batchProcessing" element={<UploadForm />} />
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,50 @@
 import React from 'react'
+import { Component } from 'react'
 import './App.css'
 
 function Home() {
     return (
         <div className='Home'>
             <h1>Home</h1>
+            <JsonForm />
         </div>
     )
 }
 
 export default Home
+
+class JsonForm extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            //Loan_ID,Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area,Loan_Status
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name] : [event.target.value]
+        })
+    }
+
+    handleSubmit(event){
+        alert(this.state.input1)
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <table><tbody>
+                        <tr><td>Gender</td><td>
+                    <input type="text" value={this.state.input1} name='input1' onChange={this.handleChange}></input></td></tr>
+                    </tbody></table>
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </div>
+        );
+    }
+}
